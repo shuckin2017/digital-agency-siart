9b6cf56afacba9cf9d3bee22aeccab2491328ec7
Maxim Schuckin <shuckin@mail.ru>
2021-08-27 01:02:14 +0300

    minor update

--- gulpfile.js
+++ gulpfile.js
@@ -1,134 +1,229 @@
-"use strict";
-
-const { src, dest } = require("gulp");
-const gulp = require("gulp");
+const { src, dest, series, watch } = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
-const cssbeautify = require("gulp-cssbeautify");
-const removeComments = require("gulp-strip-css-comments");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-const cssnano = require("gulp-cssnano");
-const rigger = require("gulp-rigger");
-const uglify = require("gulp-uglify");
-const plumber = require("gulp-plumber");
-const imagemin = require("gulp-imagemin");
+const babel = require("gulp-babel");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify-es").default;
 const del = require("del");
-const panini = require("panini");
-const browsersync = require("browser-sync").create();
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const svgSprite = require("gulp-svg-sprite");
+const fileInclude = require("gulp-file-include");
+const sourcemaps = require("gulp-sourcemaps");
+const rev = require("gulp-rev");
+const revRewrite = require("gulp-rev-rewrite");
+const revDel = require("gulp-rev-delete-original");
+const htmlmin = require("gulp-htmlmin");
+const gulpif = require("gulp-if");
+const notify = require("gulp-notify");
+const image = require("gulp-image");
+const { readFileSync } = require("fs");
+const concat = require("gulp-concat");
 
+let isProd = false; // dev by default
 
-var path = {
-  build: {
-    html: "dist/",
-    js: "dist/assets/js/",
-    css: "dist/assets/css/",
-    images: "dist/assets/img/",
-  },
-  src: {
-    html: "src/*.html",
-    js: "src/assets/js/*.js",
-    css: "src/assets/sass/main.scss",
-    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}",
-  },
-  watch: {
-    html: "src/**/*.html",
-    js: "src/assets/js/**/*.js",
-    css: "src/assets/sass/**/*.scss",
-    images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}",
-  },
-  clean: "./dist",
+const clean = () => {
+  return del(["app/*"]);
 };
 
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./dist/",
-    },
-    port: 3000,
-  });
-}
-
-function browserSyncReload(done) {
-  browsersync.reload();
-}
-
-function html() {
-  return src(path.src.html, { base: "src/" })
-    .pipe(plumber())
-    .pipe(dest(path.build.html))
-    .pipe(browsersync.stream());
-}
-
-function css() {
-  return src(path.src.css, { base: "src/assets/sass/" })
-    .pipe(plumber())
-    .pipe(sass())
+//svg sprite
+const svgSprites = () => {
+  return src("./src/img/svg/**.svg")
     .pipe(
-      autoprefixer({
-        Browserslist: ["last 8 versions"],
-        cascade: true,
-      })
-    )
-    .pipe(cssbeautify())
-    .pipe(dest(path.build.css))
-    .pipe(
-      cssnano({
-        zindex: false,
-        discardComments: {
-          removeAll: true,
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../sprite.svg", //sprite file name
+          },
         },
       })
     )
-    .pipe(removeComments())
+    .pipe(dest("./app/img"));
+};
+
+const styles = () => {
+  return src("./src/scss/**/*.scss")
+    .pipe(gulpif(!isProd, sourcemaps.init()))
+    .pipe(sass().on("error", notify.onError()))
     .pipe(
-      rename({
-        suffix: ".min",
-        extname: ".css",
+      autoprefixer({
+        cascade: false,
       })
     )
-    .pipe(dest(path.build.css))
-    .pipe(browsersync.stream());
-}
+    .pipe(gulpif(isProd, cleanCSS({ level: 2 })))
+    .pipe(gulpif(!isProd, sourcemaps.write(".")))
+    .pipe(dest("./app/css/"))
+    .pipe(browserSync.stream());
+};
 
-function js() {
-  return src(path.src.js, { base: "./src/assets/js/" })
-    .pipe(plumber())
-    .pipe(rigger())
-    .pipe(gulp.dest(path.build.js))
-    .pipe(uglify())
+const stylesBackend = () => {
+  return src("./src/scss/**/*.scss")
+    .pipe(sass().on("error", notify.onError()))
     .pipe(
-      rename({
-        suffix: ".min",
-        extname: ".js",
+      autoprefixer({
+        cascade: false,
       })
     )
-    .pipe(dest(path.build.js))
-    .pipe(browsersync.stream());
-}
+    .pipe(dest("./app/css/"));
+};
 
-function images() {
-  return src(path.src.images).pipe(imagemin()).pipe(dest(path.build.images));
-}
+const scripts = () => {
+  src("./src/js/vendor/**.js")
+    .pipe(concat("vendor.js"))
+    .pipe(gulpif(isProd, uglify().on("error", notify.onError())))
+    .pipe(dest("./app/js/"));
+  return src([
+    "./src/js/functions/**.js",
+    "./src/js/components/**.js",
+    "./src/js/main.js",
+  ])
+    .pipe(gulpif(!isProd, sourcemaps.init()))
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
+    .pipe(concat("main.js"))
+    .pipe(gulpif(isProd, uglify().on("error", notify.onError())))
+    .pipe(gulpif(!isProd, sourcemaps.write(".")))
+    .pipe(dest("./app/js"))
+    .pipe(browserSync.stream());
+};
 
-function clean() {
-  return del(path.clean);
-}
+const scriptsBackend = () => {
+  src("./src/js/vendor/**.js")
+    .pipe(concat("vendor.js"))
+    .pipe(gulpif(isProd, uglify().on("error", notify.onError())))
+    .pipe(dest("./app/js/"));
+  return src([
+    "./src/js/functions/**.js",
+    "./src/js/components/**.js",
+    "./src/js/main.js",
+  ]).pipe(dest("./app/js"));
+};
 
-function watchFiles() {
-  gulp.watch([path.watch.html], html);
-  gulp.watch([path.watch.css], css);
-  gulp.watch([path.watch.js], js);
-  gulp.watch([path.watch.images], images);
-}
+const resources = () => {
+  return src("./src/resources/**").pipe(dest("./app"));
+};
 
-const build = gulp.series(clean, gulp.parallel(html, css, js, images));
-const watch = gulp.parallel(build, watchFiles, browserSync);
+const images = () => {
+  return src([
+    "./src/img/**.jpg",
+    "./src/img/**.png",
+    "./src/img/**.jpeg",
+    "./src/img/*.svg",
+    "./src/img/**/*.jpg",
+    "./src/img/**/*.png",
+    "./src/img/**/*.jpeg",
+  ])
+    .pipe(gulpif(isProd, image()))
+    .pipe(dest("./app/img"));
+};
 
-exports.html = html;
-exports.css = css;
-exports.js = js;
-exports.images = images;
-exports.clean = clean;
-exports.build = build;
-exports.watch = watch;
-exports.default = watch;
+const htmlInclude = () => {
+  return src(["./src/*.html"])
+    .pipe(
+      fileInclude({
+        prefix: "@",
+        basepath: "@file",
+      })
+    )
+    .pipe(dest("./app"))
+    .pipe(browserSync.stream());
+};
+
+const watchFiles = () => {
+  browserSync.init({
+    server: {
+      baseDir: "./app",
+    },
+  });
+
+  watch("./src/scss/**/*.scss", styles);
+  watch("./src/js/**/*.js", scripts);
+  watch("./src/partials/*.html", htmlInclude);
+  watch("./src/*.html", htmlInclude);
+  watch("./src/resources/**", resources);
+  watch("./src/img/*.{jpg,jpeg,png,svg}", images);
+  watch("./src/img/**/*.{jpg,jpeg,png}", images);
+  watch("./src/img/svg/**.svg", svgSprites);
+};
+
+const cache = () => {
+  return src("app/**/*.{css,js,svg,png,jpg,jpeg,woff2}", {
+    base: "app",
+  })
+    .pipe(rev())
+    .pipe(revDel())
+    .pipe(dest("app"))
+    .pipe(rev.manifest("rev.json"))
+    .pipe(dest("app"));
+};
+
+const rewrite = () => {
+  const manifest = readFileSync("app/rev.json");
+  src("app/css/*.css")
+    .pipe(
+      revRewrite({
+        manifest,
+      })
+    )
+    .pipe(dest("app/css"));
+  return src("app/**/*.html")
+    .pipe(
+      revRewrite({
+        manifest,
+      })
+    )
+    .pipe(dest("app"));
+};
+
+const htmlMinify = () => {
+  return src("app/**/*.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(dest("app"));
+};
+
+const toProd = (done) => {
+  isProd = true;
+  done();
+};
+
+exports.default = series(
+  clean,
+  htmlInclude,
+  scripts,
+  styles,
+  resources,
+  images,
+  svgSprites,
+  watchFiles
+);
+
+exports.build = series(
+  toProd,
+  clean,
+  htmlInclude,
+  scripts,
+  styles,
+  resources,
+  images,
+  svgSprites,
+  htmlMinify
+);
+
+exports.cache = series(cache, rewrite);
+
+exports.backend = series(
+  toProd,
+  clean,
+  htmlInclude,
+  scriptsBackend,
+  stylesBackend,
+  resources,
+  images,
+  svgSprites
+);
